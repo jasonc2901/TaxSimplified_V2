8e14035f5eb77aaaf8f637bd8b1d04f7d7e5949f
Jason Caughers <jasonc10@hotmail.com>
2023-09-25 20:16:27 +0100

    Show calculated salary on screen

---
+++ lib/helpers/formatter.dart
@@ -0,0 +1,5 @@
+//reusable currency formatter
+import 'package:intl/intl.dart';
+
+final currencyFormatter =
+    NumberFormat.simpleCurrency(locale: 'en_GB', decimalDigits: 2);
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -5,6 +5,7 @@ import 'package:tax_simplified_new/functions/functions.dart';
 import 'package:tax_simplified_new/widgets/country_select.dart';
 import 'package:tax_simplified_new/widgets/salary_calculator.dart';
 
+import '../helpers/formatter.dart';
 import '../models/taxmodel.dart';
 import '../ui/rounded_button.dart';
 
@@ -28,133 +29,159 @@ class _HomePageState extends State<HomePage> {
       child: Scaffold(
         resizeToAvoidBottomInset: false,
         backgroundColor: darkPurple,
-        body: SalaryCalaculatorContainer(
-          backgroundColor: Colors.white,
-          widgetHeight: screenSize.height * 0.65,
-          childWidget: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: screenSize.height * 0.05,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        body: Column(
+          children: [
+            SalaryCalaculatorContainer(
+              backgroundColor: Colors.white,
+              widgetHeight: screenSize.height * 0.65,
+              childWidget: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  SizedBox(
+                    height: screenSize.height * 0.05,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Spacer(),
+                      IconButton(
+                        icon: const Icon(Icons.settings),
+                        color: darkPurple,
+                        iconSize: 35,
+                        onPressed: () {
+                          // setState(() {
+                          //   showSettings();
+                          // });
+                        },
+                      ),
+                    ],
+                  ),
+                  const Center(
+                    child: Text(
+                      "Salary Information",
+                      style: TextStyle(
+                        color: darkPurple,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 32.0,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: screenSize.height * 0.02,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Text(
+                      "Country",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        color: darkPurple,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 28.0,
+                      ),
+                    ),
+                  ),
+                  const CountrySelect(),
+                  SizedBox(
+                    height: screenSize.height * 0.02,
+                  ),
+                  const Padding(
+                    padding:
+                        EdgeInsets.only(top: 16.0, left: 16.0, right: 16.0),
+                    child: Text(
+                      "Salary per year (GBP)",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        color: darkPurple,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 28.0,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+                    child: SizedBox(
+                      width: screenSize.width * 0.45,
+                      child: TextField(
+                          style: const TextStyle(
+                            color: greyColor,
+                            fontSize: 32.0,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          controller: salaryController,
+                          cursorColor: darkPurple,
+                          decoration: const InputDecoration(
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: greyColor),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: greyColor),
+                            ),
+                          ),
+                          inputFormatters: [
+                            CurrencyTextInputFormatter(
+                              symbol: "£",
+                              decimalDigits: 0,
+                              locale: "en",
+                            )
+                          ],
+                          keyboardType: TextInputType
+                              .number // Only numbers can be entered
+                          ),
+                    ),
+                  ),
                   const Spacer(),
-                  IconButton(
-                    icon: const Icon(Icons.settings),
-                    color: darkPurple,
-                    iconSize: 35,
-                    onPressed: () {
-                      // setState(() {
-                      //   showSettings();
-                      // });
+                  RoundedButton(
+                    onPressed: () async {
+                      String salaryString = salaryController.text
+                          .replaceAll('£', '')
+                          .replaceAll(',', '');
+
+                      var calculatedModel = await calculateSalary(
+                          salaryString.isNotEmpty
+                              ? int.parse(salaryString)
+                              : 0);
+
+                      setState(() {
+                        salaryModel = calculatedModel;
+                      });
                     },
+                    text: 'Caculate',
+                    color: orangeColor,
+                    padding: screenSize.height * 0.05,
                   ),
                 ],
               ),
-              const Center(
-                child: Text(
-                  "Salary Information",
-                  style: TextStyle(
-                    color: darkPurple,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 32.0,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: screenSize.height * 0.02,
-              ),
-              const Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Text(
-                  "Country",
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    color: darkPurple,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 28.0,
-                  ),
-                ),
-              ),
-              const CountrySelect(),
-              SizedBox(
-                height: screenSize.height * 0.02,
-              ),
-              const Padding(
-                padding: EdgeInsets.only(top: 16.0, left: 16.0, right: 16.0),
-                child: Text(
-                  "Salary per year (GBP)",
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    color: darkPurple,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 28.0,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-                child: SizedBox(
-                  width: screenSize.width * 0.45,
-                  child: TextField(
-                      style: const TextStyle(
-                        color: greyColor,
-                        fontSize: 32.0,
-                        fontWeight: FontWeight.w600,
-                      ),
-                      controller: salaryController,
-                      cursorColor: darkPurple,
-                      decoration: const InputDecoration(
-                        enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: greyColor),
+            ),
+            SizedBox(
+              height: screenSize.height * 0.03,
+            ),
+            salaryModel.net > 0
+                ? Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Your yearly salary after tax is',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 26.0,
+                          ),
                         ),
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: greyColor),
+                        Text(
+                          currencyFormatter.format(salaryModel.net),
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 46.0,
+                          ),
                         ),
-                      ),
-                      inputFormatters: [
-                        CurrencyTextInputFormatter(
-                          symbol: "£",
-                          decimalDigits: 0,
-                          locale: "en",
-                        )
                       ],
-                      keyboardType:
-                          TextInputType.number // Only numbers can be entered
-                      ),
-                ),
-              ),
-              const Spacer(),
-              RoundedButton(
-                onPressed: () async {
-                  String salaryString = salaryController.text
-                      .replaceAll('£', '')
-                      .replaceAll(',', '');
-
-                  var calculatedModel = await calculateSalary(
-                      salaryString.isNotEmpty ? int.parse(salaryString) : 0);
-
-                  setState(() {
-                    salaryModel = calculatedModel;
-                  });
-                },
-                text: 'Caculate',
-                color: orangeColor,
-                padding: screenSize.height * 0.05,
-              ),
-              Container(),
-              const Spacer(),
-              Text(
-                salaryModel.net.toString(),
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30,
-                ),
-              )
-            ],
-          ),
+                    ),
+                  )
+                : Container()
+          ],
         ),
       ),
     );
